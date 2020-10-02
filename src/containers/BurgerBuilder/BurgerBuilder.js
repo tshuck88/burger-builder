742a8638077fdc85db45bc9b3f1d659c0932b5b9
Tyler Shuck <tshuck88@gmail.com>
2020-10-02 13:05:00 -0700

    created post request to save orders in firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -68,11 +69,28 @@ class BurgerBuilder extends Component {
     }
 
     purchaseCancelHandler = () => {
-        this.setState({ purchasing: false })
+        this.setState({ purchasing: false });
     }
 
     purchaseContinueHandler = () => {
-        alert("You Continue")
+        // alert("You Continue");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Tyler Shuck",
+                address: {
+                    street: "Teststreet 1",
+                    zipCode: "98119",
+                    country: "USA"
+                },
+                email: "test@test.com"
+            },
+            deliveryMethod: "fastest"
+        }
+        axios.post("/orders.json", order)
+            .then(response => console.log(response))
+            .catch(err => console.log(err));
     }
 
     render() {
