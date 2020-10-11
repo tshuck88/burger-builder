e3a5148ab1318e4b7676357e91422f557d87d92c
Tyler Shuck <tshuck88@gmail.com>
2020-10-10 18:04:24 -0700

    created handlers to continue or cancel checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,11 +10,22 @@ class Checkout extends Component {
             bacon: 1
         }
     }
+
+    checkoutCancelled = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinued = () => {
+        this.props.history.replace("/checkout/contact-data");
+    }
+
     render() {
         return (
             <div>
                 <CheckoutSummary
-                    ingredients={this.state.ingredients} />
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelled}
+                    checkoutContinued={this.checkoutContinued} />
             </div>
         )
     }
