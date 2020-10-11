6761a3db96cd7b33cd299eab56b567de9eda31b5
Tyler Shuck <tshuck88@gmail.com>
2020-10-10 18:35:45 -0700

    created contact data component and route

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import { Route } from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
     state = {
@@ -35,6 +37,9 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelled}
                     checkoutContinued={this.checkoutContinued} />
+                <Route
+                    path={this.props.match.path + "/contact-data"}
+                    component={ContactData} />
             </div>
         )
     }
