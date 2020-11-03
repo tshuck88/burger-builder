c478f83d1a1a9ddac2358ab89ddb85c9985d94c1
Tyler Shuck <tshuck88@gmail.com>
2020-11-03 13:49:52 -0800

    wrote second test if authenticated

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -6,8 +6,17 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
     it('should render two <NavigationItems /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render three <NavigationItems /> elements if authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true })
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
